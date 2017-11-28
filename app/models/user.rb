70c819d60742ece0ba2aa89d112b4a885065bd0a
Renato Rosa Franco <renato_ny@live.com>
2017-11-28 13:52:42 -0200

    Database Diagram added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  self.table_name ='users'
+  self.primary_key = 'id'
+
+  mount_uploader :avatar, FileUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
